23af6ebe5bbdc4a9427bcd370259d56234aba4a6
anxiousscripter <179371895+anxiousscripter@users.noreply.github.com>
2026-09-05 19:18:07 +0300

    New Feature: Evaluate single pair values
    Added functionality to evaluate a single pair of values at a time and
    use the result as input for the next evaluation.
    ---Fixes---
    - Calculation starting with operator: Screen is set to update only if
    the first button clicked is a number button.
    - Result concatenation: Result field displays correct calculated value
    instead of appending new results to previous calculation results.

--- js/main.js
+++ js/main.js
@@ -11,26 +11,53 @@ let operator;
 
 buttons.map((element) => {
 	element.addEventListener('click', () => {
+		// if the button clicked is a number
 		if (element.classList.contains('btn-number')) {
+			// display that number
 			inputField.textContent += element.textContent;
 		}
 
+		// if the button clicked is an operator, is not the equals sign, and there is no other operator present
 		if (
 			!inputField.textContent.includes(' ') &&
 			!element.classList.contains('btn-equals') &&
 			element.classList.contains('btn-operator')
 		) {
-			inputField.textContent += ` ${element.textContent} `;
+			// if the input field is not empty
+			if (inputField.textContent !== '') {
+				// display that operator next to whatever is already in the display
+				inputField.textContent += ` ${element.textContent} `;
+			}
+
+			// however, if there already is an operator present and the other two conditions are met
+		} else if (
+			inputField.textContent.includes(' ') &&
+			!element.classList.contains('btn-equals') &&
+			element.classList.contains('btn-operator')
+		) {
+			// get whatever is being displayed and split it into num1, the operator, and num2
+			const splitInput = inputField.textContent.split(' ');
+			// convert num1 and num2 to numbers and assign each value to its respective variable
+			num1 = Number(splitInput[0]);
+			operator = splitInput[1];
+			num2 = Number(splitInput[2]);
+			// call the operate function and display whatever it returns in the result field
+			resultField.textContent = operate(num1, operator, num2);
+			// update the input field to display the result of the previous calculation as num1
+			// and to use the operator selected as the operator for the next calculation
+			inputField.textContent = `${resultField.textContent} ${element.textContent} `;
 		}
 
+		// if the button clicked is the equals sign
 		if (element.classList.contains('btn-equals')) {
 			const splitInput = inputField.textContent.split(' ');
 			num1 = Number(splitInput[0]);
 			operator = splitInput[1];
 			num2 = Number(splitInput[2]);
-			resultField.textContent += operate(num1, operator, num2);
+			resultField.textContent = operate(num1, operator, num2);
 		}
 
+		// if the button clicked is the clear button
 		if (element.classList.contains('btn-clear')) {
 			num1 = 0;
 			num2 = 0;
